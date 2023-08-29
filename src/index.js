3eeb5612df49af04a62a93d1fb9d4c6e5daec6a9
K3PAA <jkepa00@gmail.com>
2023-08-29 16:55:59 +0200

    React/SASS webpack setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Home from './Home'
+import './App.scss'
+
+const el = document.getElementById('app')
+
+ReactDOM.render(<Home />, el)
